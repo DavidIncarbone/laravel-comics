cc556988e6c947df2ae2a4797be37dcd9942773d
DavidIncarbone <david.incarbone@gmail.com>
2025-03-21 16:38:27 +0100

    add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,19 @@
-<header class="debug">
-    <nav class="container wrapper"> I nostri vari link </nav>
+<header class="">
+    <nav class="header-nav container wrapper d-flex justify-content-between align-items-center py-2 "> 
+        <div class="header-img-container">
+            <img src="{{Vite::asset("resources/img/dc-logo.png")}}" alt="">
+        </div>
+        <ul class="list-group flex-row gap-3 ">
+            <li class=""><a href="">characters</a></li>
+            <li class=""><a href="">comics</a></li>
+            <li class=""><a href="">movies</a></li>
+            <li class=""><a href="">tv</a></li>
+            <li class=""><a href="">games</a></li>
+            <li class=""><a href="">collectibles</a></li>
+            <li class=""><a href="">videos</a></li>
+            <li class=""><a href="">fans</a></li>
+            <li class=""><a href="">news</a></li>
+            <li class=""><a href="">shop</a></li>
+        </ul>
+         </nav>
 </header>
